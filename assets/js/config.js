3943bca91504f9c4979c5ed312e81fb363c1f49d
MajitoCupcake <maryjosehf@gmail.com>
2025-10-28 17:56:04 -0600

    Mejora 3

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
 export const API_BASE = "http://18.207.21.168:5500/api";
-export const WS_URL  = "ws://18.207.21.168:5500/ws";
+export const WS_URL = "ws://18.207.21.168:5500/ws";
 export const DEFAULT_DEVICE = "Majo";
